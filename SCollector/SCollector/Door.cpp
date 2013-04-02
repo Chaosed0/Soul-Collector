a85e4e53ac9643e8a48279534a30d70526eec30c
1990mustafa <1990mustafa>
2013-04-02 13:33:44 -0400

    door class

--- SCollector/SCollector/Door.cpp
+++ SCollector/SCollector/Door.cpp
@@ -1,16 +1,50 @@
-#include "door.h"
+#include "Door.h"
 #include "Entity.h"
 #include "Level.h"
 #include "Player.h"
 
-int Door::CurrentID = 0;
+Door::Door(const sf::Vector2f& pos)
+	: Activatable(" ", sf::IntRect(0,0,32,32), sf::IntRect(0,0,32,32))
+{
+	health = 100.0;
+	isOpened = false;
+	isBroken = false;
+	SetPos(pos);
+}
+
 
 void Door::Break()
 {
+	if(this->health == 0)
+		isBroken = true;
 }
 
-Door::Door()
+void Door::Open(std::list<Key> listKeys)
 {
+	std::list<Key>::iterator it = listKeys.begin();
+
+	while (it != listKeys.end() )
+	{
+		if (it->getKeyID == this->doorID)
+		{
+			isOpened = true;
+			break;
+		}
+		else
+		{	
+			it++;
+		}
+	}
+}
+
+double Door::GetHealth() const
+{
+	return health;
+}
+
+bool Door::IsOpened() const
+{
+	return isOpened;
 }
 
 // this functions to create door and key with IDs
@@ -18,21 +52,29 @@ Door *Door::Instantiate(Key *key, const sf::Vector2f& pos)	//Key *key;
 {															//Door *door = Door::Instantiate(key);
 	Door *newDoor = new Door();
 
-	newDoor->ID = CurrentID++;
+	newDoor->doorID = CurrentID++;
 
 	key = new Key(pos);
 
-	key->setKeyID(newDoor->ID);
+	key->setKeyID(newDoor->doorID);
 
 	return newDoor;
 }
 
 // this functions to create door and key with IDs
-Key *Door::getNewKey(const sf::Vector2f& pos)		//Door *door = new Door();
+Key *Door::GetNewKey(const sf::Vector2f& pos) const //Door *door = new Door();
 {													//Key *key = door->getNewKey();
 	Key *key = new Key(pos);
 
-	key->setKeyID(this->ID);
+	key->setKeyID(this->doorID);
 
 	return key;
+}
+
+void Door::Update(Level& level, const sf::Time& timePassed)
+{
+	if(IsActive() && !IsFinished()) {
+		visible = false;
+		Finish();
+	}
 }
--- SCollector/SCollector/Door.h
+++ SCollector/SCollector/Door.h
@@ -2,29 +2,35 @@
 #define __DOOR_H
 
 #include <string>
+#include <list>
 #include "key.h"
+#include "Activatable.h"
 
-class Door
+class Door : public Activatable
 {
 public:
-	static Door *Instantiate(Key *key, const sf::Vector2f& pos);	//set up Door and its key
-
-	static int CurrentID;
-	
-	void Break();
+	static int Door::CurrentID; 
 
 	Door();
+	Door(const sf::Vector2f& pos);
+	Door *Instantiate(Key *key, const sf::Vector2f& pos);	//set up Door and its key
+	
+	void Break();
+	void Open(std::list<Key> listKeys);
 
-	Key *getNewKey(const sf::Vector2f& pos);
-
-
+	Key *GetNewKey(const sf::Vector2f& pos) const;
+	double GetHealth() const; 
+	bool IsOpened() const;
+	
+	virtual void Update(Level& level, const sf::Time& timePassed);
 private:
+	
 	std::string doorName;
 	int health;
 	bool isOpened;
 	bool isBroken;
 
-	int ID;				// door ID
+	int doorID;				// door ID
 };
 
 #endif
