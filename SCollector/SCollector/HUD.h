b57794ea1cf5da4c864425e79af04eac0ea5e5f4
unknown <cml5443@P220INST19.cse.psu.edu>
2013-04-21 14:17:12 -0400

    updated map

--- SCollector/SCollector/HUD.h
+++ SCollector/SCollector/HUD.h
@@ -1,3 +1,9 @@
+/**
+ * \brief The HUD, which contains HudObjects and a text string
+ * \author Chris Lawyer
+ * \Date March 21 2013
+ */
+
 #ifndef HUD_H
 #define HUD_H
 
@@ -8,6 +14,7 @@
 class HUD
 {
 public:
+	//Constructor
 	HUD(const sf::Vector2f& pos);
 
 	/**
@@ -16,11 +23,28 @@ public:
 	 */
 	void AddObject(const std::string& imgLoc);
 
+	/**
+	 * Changes the fullness of the bar for a single HudObject
+	 * \param final The target final value of the bar (0-100)
+	 * \param which Indicator as to which HudObject to change (0 is the leftmost object)
+	 */
 	void changeFill(int final, int which);
+
+	/**
+	 * Changes the text of the HUD's textObject
+	 * \param text The new value of the text of textObject
+	 */
 	void changeText(std::string text);
 
+	/**
+	 * Draws the HUD to the window as well as its HudObjects and textObject
+	 * \param window The window to be drawn to
+	 */
 	void draw(sf::RenderWindow& window) const;
 
+	/**
+	 * The values associated with the HudObjects
+	 */
 	static const enum {
 		health = 0,
 		fuel,
@@ -28,18 +52,28 @@ public:
 		fatigue
 	};
 private:
+	//Vector of HudObject pointers to point to the HudObjects in the HUD
 	std::vector<HudObject *> objects;
 
+	//The font used for the textObject
 	sf::Font font;
+	//Text to be displayed on the screen above the HUD
 	sf::Text textObject;
+	//Clock to keep track of time for the textObject
 	sf::Clock clock;
 
+	//The current x position to draw HudObjects side-by-side
 	float curXPos;
+	//Value to keep track of previous humanity changes
 	float prevHumanityChange;
+	//The position in vector form 
 	sf::Vector2f pos;
 
+	//The amount of time text should be displayed on the screen by the textObject
 	static const sf::Time textDisplayTime;
+	//The distance in pixels between the text for the textObject and the HudObjects
 	static const int textPadding;
+	//The value at which the humanity object will change pictures
 	static const int humanityIncrement;
 };
 
